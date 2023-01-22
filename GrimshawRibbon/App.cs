a6a047492be678669aea46f702f68cf9f9dc733d
ivaylo-matov <ivaylo.matov@gmail.com>
2023-01-22 14:15:58 +0000

    BUTTONS DONE !!!

--- GrimshawRibbon/App.cs
+++ GrimshawRibbon/App.cs
@@ -27,41 +27,38 @@ namespace GrimshawRibbon
             // Get dll assembly path
             string thisAssemblyPath = Assembly.GetExecutingAssembly().Location;
 
-            // create push buttons for PurgeCADImport
-            PulldownButtonData pbg1Data = new PulldownButtonData("DropdownGrouop1", "Purge Pluss");
+            // create pushdown button for Purge +
+            PulldownButtonData pbg1Data = new PulldownButtonData("DropdownGrouop1", "Purge +");
             PulldownButton pbg1 = ribbonPanel.AddItem(pbg1Data) as PulldownButton;
             pbg1.ToolTip = "Collection of tools to purge various elements that cannot be purged by the OOTB purge tool.";
 
-            BitmapImage pbg0Image = new BitmapImage(new Uri("pack://application:,,,/GrimshawRibbon;component/Resources/PurgePlusPushutton.png"));
-            pbg1.LargeImage = pbg0Image;
+            // create image for all icons
+            BitmapImage purgeIcon = new BitmapImage(new Uri("pack://application:,,,/GrimshawRibbon;component/Resources/PurgePlusPushdown.png"));
+            pbg1.LargeImage = purgeIcon;
 
-
+            // create the push buttons
             PushButtonData b1Data = new PushButtonData("cmdPurgeCADImport1", "Purge CAD Imports1", thisAssemblyPath, "Kipple.PurgeCADImports");
             PushButton pb1 = pbg1.AddPushButton(b1Data) as PushButton;
             pb1.ToolTip = "This is tool 1.";
-            BitmapImage pb1Image = new BitmapImage(new Uri("pack://application:,,,/GrimshawRibbon;component/Resources/PurgePlus1.png"));
-            pb1.LargeImage = pb1Image;
-           
+            pb1.LargeImage = purgeIcon;
+
+
 
             PushButtonData b2Data = new PushButtonData("cmdPurgeCADImport2", "Purge CAD Imports2", thisAssemblyPath, "Kipple.PurgeCADImports");
             PushButton pb2 = pbg1.AddPushButton(b2Data) as PushButton;
             pb2.ToolTip = "This is tool 2.";
-            BitmapImage pb2Image = new BitmapImage(new Uri("pack://application:,,,/GrimshawRibbon;component/Resources/PurgePlus2.png"));
-            pb2.LargeImage = pb2Image;
+            pb2.LargeImage = purgeIcon;
 
             PushButtonData b3Data = new PushButtonData("cmdPurgeCADImport3", "Purge CAD Imports3", thisAssemblyPath, "Kipple.PurgeCADImports");
             PushButton pb3 = pbg1.AddPushButton(b3Data) as PushButton;
             pb3.ToolTip = "This is tool 3.";
-            BitmapImage pb3Image = new BitmapImage(new Uri("pack://application:,,,/GrimshawRibbon;component/Resources/PurgePlus3.png"));
-            pb3.LargeImage = pb3Image;
+            pb3.LargeImage = purgeIcon;
 
             PushButtonData b4Data = new PushButtonData("cmdPurgeCADImport4", "Purge CAD Imports4", thisAssemblyPath, "Kipple.PurgeCADImports");
             PushButton pb4 = pbg1.AddPushButton(b4Data) as PushButton;
             pb4.ToolTip = "This is tool 4.";
-            BitmapImage pb4Image = new BitmapImage(new Uri("pack://application:,,,/GrimshawRibbon;component/Resources/PurgePlus4.png"));
-            pb4.LargeImage = pb4Image;
+            pb4.LargeImage = purgeIcon;
 
-            //pbg1.CurrentButton = pb1;
         }
 
         public Result OnShutdown(UIControlledApplication application)
